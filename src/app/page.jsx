84cfaa3abe62f1232814720aa07ce6afeff09e5c
Ebba-Ene <eneqvist.ebba@gmail.com>
2026-09-07 13:41:08 +0200

    Initial Pages and folders

---
+++ src/app/page.jsx
@@ -0,0 +1,18 @@
+import { getStoryblokApi } from "@/lib/storyblok"
+import { StoryblokStory } from "@storyblok/react/rsc";
+
+export default async function HomePage() {
+  let story
+
+  try{
+    const storyblokApi = getStoryblokApi()
+    const { data } = await storyblokApi.get("cdn/stories/home", {
+      version: "draft",
+    });
+    story = data.story;
+  } catch {
+    notFound();
+  }
+
+  return <StoryblokStory story={story} />;
+}
