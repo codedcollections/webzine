65c7de5c90840054d829feeda6075097718ed660
codedcollections <ma.unander@gmail.com>
2026-09-08 11:39:01 +0200

    Add author slug page with details about authors

---
+++ src/app/authors/[slug]/page.jsx
@@ -0,0 +1,20 @@
+import { getStoryblokApi } from '@/lib/storyblok';
+import { StoryblokServerComponent } from '@storyblok/react/rsc';
+import { notFound } from 'next/navigation';
+
+export default async function AuthorPage({ params }) {
+	const { slug } = await params;
+	const storyblokApi = getStoryblokApi();
+
+	let story;
+	try {
+		const { data } = await storyblokApi.get(`cdn/stories/authors/${slug}`, {
+			version: 'draft',
+		});
+		story = data.story;
+	} catch {
+		notFound();
+	}
+
+	return <StoryblokServerComponent blok={story.content} />;
+}
