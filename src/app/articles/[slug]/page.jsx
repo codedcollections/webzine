9f164013b5e079b46c99e5f6c34b3e79c9985bf5
Ebba-Ene <eneqvist.ebba@gmail.com>
2026-09-08 11:35:00 +0200

    add generateMetadata to articles/[slug]

--- src/app/articles/[slug]/page.jsx
+++ src/app/articles/[slug]/page.jsx
@@ -2,6 +2,24 @@ import { getStoryblokApi } from "@/lib/storyblok"
 import { StoryblokServerComponent } from "@storyblok/react/rsc"
 import { notFound } from "next/navigation"
 
+export async function generateMetadata({params}) {
+  const {slug} = await params
+  const storyblokApi = getStoryblokApi()
+
+  const {data} = await storyblokApi.get(`cdn/stories/articles/${slug}`, {
+    version: "draft"
+  })
+
+  const story = data.story
+
+  console.log("Story: ", story)
+
+  return{
+    title: story.content.title,
+    description: story.content.summary
+  }
+}
+
 export default async function ArticlePostPage ({params}) {
   const {slug} = await params
   const storyblokApi = getStoryblokApi()
