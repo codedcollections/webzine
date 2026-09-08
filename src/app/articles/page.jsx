f71feed4eb921e9169565c44ab709bee94ec2e8d
Ebba-Ene <eneqvist.ebba@gmail.com>
2026-09-08 10:07:27 +0200

    add article index page

---
+++ src/app/articles/page.jsx
@@ -0,0 +1,34 @@
+import { getStoryblokApi } from "@/lib/storyblok";
+import { StoryblokStory } from "@storyblok/react/rsc";
+import { notFound } from "next/navigation";
+
+export async function generateMetadata() {
+  const storyblokApi = getStoryblokApi()
+
+  const {data} = await storyblokApi.get("cdn/stories/articles", {
+    version: "draft"
+  })
+
+  const content = data.story.content
+
+  return{
+    title: content.seo_title || data.story.name,
+    description: "Hejhej"
+  }
+}
+
+export default async function ArticlesPage() {
+  let story;
+
+  try {
+    const storyblokApi = getStoryblokApi()
+    const {data} = await storyblokApi.get("cdn/stories/articles", {
+      version: "draft"
+    })
+    story = data.story
+  } catch{
+    notFound()
+  }
+
+  return <StoryblokStory story={story}/>
+}
