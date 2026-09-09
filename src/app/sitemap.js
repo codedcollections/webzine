2dd48e05ae358f9f73f523fb53ffee3778cf5668
Desirée Strand <desireestrxnd@gmail.com>
2026-09-09 12:37:09 +0200

    filter out broken url

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,27 +1,32 @@
-import { getStoryblokApi } from "@/lib/storyblok";
+import { getStoryblokApi } from '@/lib/storyblok';
 
-const siteUrl = process.env.SITE_URL ?? "https://localhost:3000";
+const siteUrl = process.env.SITE_URL ?? 'https://localhost:3000';
 
 export default async function sitemap() {
-  const storyblokApi = getStoryblokApi();
+	const storyblokApi = getStoryblokApi();
 
-  const { data } = await storyblokApi.getStories({
-    version: "published",
-    per_page: 100,
-  });
+	const { data } = await storyblokApi.getStories({
+		version: 'published',
+		per_page: 100,
+	});
 
-  const pages = data.stories
-    .filter((story) => !story.full_slug.startsWith("config"))
-    .map((story) => ({
-      url: `${siteUrl}/${story.full_slug}`,
-      lastModified: story.published_at ?? new Date(),
-    }));
+	const pages = data.stories
+		.filter(
+			(story) =>
+				!story.full_slug.startsWith('config') &&
+				story.full_slug !== 'home' &&
+				story.full_slug !== 'category-page',
+		)
+		.map((story) => ({
+			url: `${siteUrl}/${story.full_slug}`,
+			lastModified: story.published_at ?? new Date(),
+		}));
 
-  return [
-    {
-      url: siteUrl,
-      lastModified: new Date(),
-    },
-    ...pages,
-  ];
+	return [
+		{
+			url: siteUrl,
+			lastModified: new Date(),
+		},
+		...pages,
+	];
 }
