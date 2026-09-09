13ae00a54e8e4c751216248abe20506200244aa0
Desirée Strand <desireestrxnd@gmail.com>
2026-09-09 12:39:01 +0200

    change header title to uppercase

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ export default function Header({ blok }) {
 			className="sticky top-0 z-40 bg-white border-b border-gray-200 px-4 flex items-stretch justify-between h-20"
 			{...storyblokEditable(blok)}
 		>
-			<h2 className="text-3xl font-bold tracking-tight text-gray-900 capitalize self-center">
+			<h2 className="text-3xl font-bold tracking-tight text-gray-900 uppercase self-center">
 				{blok.headerTitle}
 			</h2>
 			<nav className="flex h-full">
